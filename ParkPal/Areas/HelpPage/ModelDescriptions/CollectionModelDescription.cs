5ba73c203cf0816a3ca9c7425bdde26cf471a491
MagenR <83386524+MagenR@users.noreply.github.com>
2022-04-21 15:12:30 +0300

    Delete ParkPal directory
    Needs to reupload.

--- ParkPal/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ParkPal.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
